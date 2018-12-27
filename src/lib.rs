f287194c9e28ca51cc6a8693a8f16da54e4bce5f
Rahul Garg <vihu89@gmail.com>
2018-12-27 10:35:58 -0800

    More WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use openssl::bn::{BigNum, BigNumContext};
 use openssl::error::ErrorStack;
 
-pub struct PedersenCommitment {
+// Why I do what I do, I don't know
+pub struct Verifier {
     pub p: BigNum,
     pub q: BigNum,
     pub g: BigNum,
@@ -9,8 +10,16 @@ pub struct PedersenCommitment {
     pub ctx: BigNumContext
 }
 
-impl PedersenCommitment {
-    pub fn new(security: i32) -> Result< PedersenCommitment, ErrorStack > {
+pub struct Prover {
+    pub p: BigNum,
+    pub q: BigNum,
+    pub g: BigNum,
+    pub h: BigNum,
+    pub ctx: BigNumContext
+}
+
+impl Verifier {
+    pub fn new(security: i32) -> Result< Verifier, ErrorStack > {
 
         // create context to manage the bignum
         let mut ctx = BigNumContext::new()?;
@@ -47,7 +56,7 @@ impl PedersenCommitment {
         let mut h = BigNum::new()?;
         h.mod_exp(&g, &alpha, &p, &mut ctx)?;
 
-        Ok(PedersenCommitment {
+        Ok(Verifier {
             p: p,
             q: q,
             g: g,
@@ -55,5 +64,59 @@ impl PedersenCommitment {
             ctx: ctx
         })
     }
+
+    pub fn add(&mut self, cm: &[BigNum]) -> Result< BigNum, ErrorStack> {
+        // XXX: this is most definitely *wrong*
+        let one = BigNum::from_u32(1)?;
+        let mut res = BigNum::new()?;
+        for c in cm {
+            res.checked_mul(&one, &c, &mut self.ctx)?;
+        }
+        let mut tmp = BigNum::new()?;
+        tmp.nnmod(&res, &self.q, &mut self.ctx)?;
+        Ok(tmp)
+    }
+
+    // pub fn open(&mut self, c: BigNum, x: BigNum, r: &[BigNum]) -> Result< BigNum, ErrorStack > {
+    //     // Do this
+    // }
 }
 
+impl Prover {
+    pub fn new(p: BigNum,
+               q: BigNum,
+               g: BigNum,
+               h: BigNum,
+               ctx: BigNumContext) -> Result< Prover, ErrorStack > {
+        Ok(Prover {
+            p: p,
+            q: q,
+            g: g,
+            h: h,
+            ctx: ctx
+        })
+    }
+
+    pub fn commit(&mut self, x: u32) -> Result <(BigNum, BigNum), ErrorStack> {
+        let one = BigNum::from_u32(1)?;
+        // generate random number between 1, q-1
+        let r = BigNum::new()?;
+        let mut tmp1 = BigNum::new()?;
+        tmp1.checked_sub(&self.p, &one)?;
+        let mut tmp2 = BigNum::new()?;
+        tmp2.checked_add(&r, &one)?;
+        r.rand_range(&mut tmp1)?;
+
+        // c: calculate commitment
+        let x1 = BigNum::from_u32(x)?;
+        let mut c = BigNum::new()?;
+        let mut tmp3 = BigNum::new()?;
+        let mut tmp4 = BigNum::new()?;
+        tmp3.mod_exp(&self.g, &x1, &self.q, &mut self.ctx)?;
+        tmp4.mod_exp(&self.h, &r, &self.q, &mut self.ctx)?;
+        c.mod_mul(&tmp3, &tmp4, &self.q, &mut self.ctx)?;
+
+        Ok((c, r))
+
+    }
+}
