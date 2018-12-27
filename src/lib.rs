0ff9b1c101262b4252af122b88f544a932c26e49
Jay Kickliter <jay@kickliter.com>
2018-12-27 14:07:16 -0800

    Fix order of operations when calling rand_range

--- src/lib.rs
+++ src/lib.rs
@@ -39,20 +39,20 @@ impl PedersenCommitment {
         q.checked_add(&tmp, &one)?;
 
         // generate random BigNum between 1, p-1
-        let g = BigNum::new()?;
+        let mut g = BigNum::new()?;
         let mut tmp2 = BigNum::new()?;
         tmp2.checked_sub(&p, &one)?;
         let mut tmp3 = BigNum::new()?;
         tmp3.checked_add(&g, &one)?;
-        g.rand_range(&mut tmp2)?;
+        tmp2.rand_range(&mut g)?;
 
         // generate secret alpha between 1, p-1
-        let alpha = BigNum::new()?;
+        let mut alpha = BigNum::new()?;
         let mut tmp4 = BigNum::new()?;
         tmp4.checked_sub(&p, &one)?;
         let mut tmp5 = BigNum::new()?;
         tmp5.checked_add(&g, &one)?;
-        alpha.rand_range(&mut tmp4)?;
+        tmp4.rand_range(&mut alpha)?;
 
         // calculate h = pow(g, alpha, p)
         let mut h = BigNum::new()?;
