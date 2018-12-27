4d4ae98ff8e28536408161948bd12ad6c08f43fb
Rahul Garg <vihu89@gmail.com>
2018-12-27 10:51:00 -0800

    Moar WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use openssl::bn::{BigNum, BigNumContext};
 use openssl::error::ErrorStack;
 
 // Why I do what I do, I don't know
-pub struct Verifier {
+pub struct PedersenCommitment {
     pub p: BigNum,
     pub q: BigNum,
     pub g: BigNum,
@@ -10,16 +10,8 @@ pub struct Verifier {
     pub ctx: BigNumContext
 }
 
-pub struct Prover {
-    pub p: BigNum,
-    pub q: BigNum,
-    pub g: BigNum,
-    pub h: BigNum,
-    pub ctx: BigNumContext
-}
-
-impl Verifier {
-    pub fn new(security: i32) -> Result< Verifier, ErrorStack > {
+impl PedersenCommitment {
+    pub fn new(security: i32) -> Result< PedersenCommitment, ErrorStack > {
 
         // create context to manage the bignum
         let mut ctx = BigNumContext::new()?;
@@ -56,7 +48,7 @@ impl Verifier {
         let mut h = BigNum::new()?;
         h.mod_exp(&g, &alpha, &p, &mut ctx)?;
 
-        Ok(Verifier {
+        Ok(PedersenCommitment {
             p: p,
             q: q,
             g: g,
@@ -64,59 +56,46 @@ impl Verifier {
             ctx: ctx
         })
     }
-
-    pub fn add(&mut self, cm: &[BigNum]) -> Result< BigNum, ErrorStack> {
-        // XXX: this is most definitely *wrong*
-        let one = BigNum::from_u32(1)?;
-        let mut res = BigNum::new()?;
-        for c in cm {
-            res.checked_mul(&one, &c, &mut self.ctx)?;
-        }
-        let mut tmp = BigNum::new()?;
-        tmp.nnmod(&res, &self.q, &mut self.ctx)?;
-        Ok(tmp)
-    }
-
-    // pub fn open(&mut self, c: BigNum, x: BigNum, r: &[BigNum]) -> Result< BigNum, ErrorStack > {
-    //     // Do this
-    // }
 }
 
-impl Prover {
-    pub fn new(p: BigNum,
-               q: BigNum,
-               g: BigNum,
-               h: BigNum,
-               ctx: BigNumContext) -> Result< Prover, ErrorStack > {
-        Ok(Prover {
-            p: p,
-            q: q,
-            g: g,
-            h: h,
-            ctx: ctx
-        })
+// fn pedersen_open(cmt: &mut PedersenCommitment, c: BigNum, x: u32, args: &[BigNum]) -> Result< BigNum, ErrorStack > {
+//     let total = BigNum::new()?;
+//
+//     for e in args {
+//         res.checked_add(&total, )
+//     }
+// }
+
+fn pedersen_add(cmt: &mut PedersenCommitment, cm: &[BigNum]) -> Result< BigNum, ErrorStack> {
+    // XXX: this is most definitely *wrong*
+    let one = BigNum::from_u32(1)?;
+    let mut res = BigNum::new()?;
+    for c in cm {
+        res.checked_mul(&one, &c, &mut cmt.ctx)?;
     }
+    let mut tmp = BigNum::new()?;
+    tmp.nnmod(&res, &cmt.q, &mut cmt.ctx)?;
+    Ok(tmp)
+}
 
-    pub fn commit(&mut self, x: u32) -> Result <(BigNum, BigNum), ErrorStack> {
-        let one = BigNum::from_u32(1)?;
-        // generate random number between 1, q-1
-        let r = BigNum::new()?;
-        let mut tmp1 = BigNum::new()?;
-        tmp1.checked_sub(&self.p, &one)?;
-        let mut tmp2 = BigNum::new()?;
-        tmp2.checked_add(&r, &one)?;
-        r.rand_range(&mut tmp1)?;
+fn pedersen_commit(cmt: &mut PedersenCommitment, x: u32) -> Result <(BigNum, BigNum), ErrorStack> {
+    let one = BigNum::from_u32(1)?;
+    // generate random number between 1, q-1
+    let r = BigNum::new()?;
+    let mut tmp1 = BigNum::new()?;
+    tmp1.checked_sub(&cmt.p, &one)?;
+    let mut tmp2 = BigNum::new()?;
+    tmp2.checked_add(&r, &one)?;
+    r.rand_range(&mut tmp1)?;
 
-        // c: calculate commitment
-        let x1 = BigNum::from_u32(x)?;
-        let mut c = BigNum::new()?;
-        let mut tmp3 = BigNum::new()?;
-        let mut tmp4 = BigNum::new()?;
-        tmp3.mod_exp(&self.g, &x1, &self.q, &mut self.ctx)?;
-        tmp4.mod_exp(&self.h, &r, &self.q, &mut self.ctx)?;
-        c.mod_mul(&tmp3, &tmp4, &self.q, &mut self.ctx)?;
+    // c: calculate commitment
+    let x1 = BigNum::from_u32(x)?;
+    let mut c = BigNum::new()?;
+    let mut tmp3 = BigNum::new()?;
+    let mut tmp4 = BigNum::new()?;
+    tmp3.mod_exp(&cmt.g, &x1, &cmt.q, &mut cmt.ctx)?;
+    tmp4.mod_exp(&cmt.h, &r, &cmt.q, &mut cmt.ctx)?;
+    c.mod_mul(&tmp3, &tmp4, &cmt.q, &mut cmt.ctx)?;
 
-        Ok((c, r))
-
-    }
+    Ok((c, r))
 }
